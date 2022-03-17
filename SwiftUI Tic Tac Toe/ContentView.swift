674a9d4b577b7b56346f26208b4a8431694b32a0
David W. Brown <davidwbrown@hey.com>
2022-03-17 18:00:50 -0500

    Initial Commit

---
+++ SwiftUI Tic Tac Toe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI Tic Tac Toe
+//
+//  Created by David W. Brown on 3/17/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI Tic Tac Toe/SwiftUI_Tic_Tac_ToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Tic_Tac_ToeApp.swift
+//  SwiftUI Tic Tac Toe
+//
+//  Created by David W. Brown on 3/17/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Tic_Tac_ToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
